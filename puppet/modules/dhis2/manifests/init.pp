a7c736b1469e5d605eb28aa1c6ef9d4f79f30760
Chris Ford <christophertford@gmail.com>
2012-12-19 17:30:33 +0300

    Cleaned up manifests.

--- puppet/modules/dhis2/manifests/init.pp
+++ puppet/modules/dhis2/manifests/init.pp
@@ -1,10 +1,5 @@
 class dhis2 {
-  package { "openjdk-7-jdk":
+  package { "unzip":
     ensure => present,
   }
-
-#  exec { "jar":
-#    command => "/usr/bin/wget http://apps.dhis2.org/ci/job/dhis-web-trunk/lastSuccessfulBuild/artifact/dhis-2/dhis-web/dhis-web-portal/target/dhis.war -o dhis.war",
-#    refreshonly => true,
-#  }
 }
--- puppet/modules/postgres/manifests/init.pp
+++ puppet/modules/postgres/manifests/init.pp
@@ -11,26 +11,13 @@ class postgres {
   }
 
   file { "pg_hba.conf" :
-      require => Package["postgresql"],
-      path => "/etc/postgresql/9.1/main/pg_hba.conf",
-      ensure => file,
-      owner => postgres,
-      group => postgres,
-      mode => 664,
-      replace => true,
-      content => template("postgres/pg_hba.conf.erb"),
-  }
-
-  exec { "createuser":
-    require => Service["postgresql"],
-    command => "/usr/bin/sudo -u postgres -c \"/usr/bin/createuser -DRS dhis2\"",
-#    refreshonly => true,
-  }
-
-  exec { "createdb":
-    require => Exec["createuser"],
-    require => Service["postgresql"],
-    command => "/usr/bin/sudo -u postgres -c \"/usr/bin/createdb -O postgres dhis2\"",
-#    refreshonly => true,
+    require => Package["postgresql"],
+    path => "/etc/postgresql/9.1/main/pg_hba.conf",
+    ensure => file,
+    owner => postgres,
+    group => postgres,
+    mode => 664,
+    replace => true,
+    content => template("postgres/pg_hba.conf.erb"),
   }
 }
